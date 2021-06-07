64264f0596803a41f3674fb0718231d5de473813
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2021-06-07 20:28:36 -0300

    desafio completo

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
+    port: 5500
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),  
